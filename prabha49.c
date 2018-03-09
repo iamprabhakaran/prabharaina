1e56e395e201e3161903c3cf082134faa9dc6ebe
iamprabhakaran <35688582+iamprabhakaran@users.noreply.github.com>
2018-03-09 13:12:31 +0530

    Create prabha49.c

---
+++ prabha49.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main(void)
+
+{
+	int m,n,sum;
+	scanf("%d",&m);
+	scanf("%d",&n);
+	sum=m+n;
+	printf("%d",sum);
+	return 0;
+
+}                                         
